ed997b2ba7152f586608e304f9deae835dbc74eb
Maria LaRocca <mclarocca@gmail.com>
2020-06-27 16:14:01 -0400

    Install and use form validation before submit

--- form.js
+++ form.js
@@ -1,54 +1,39 @@
-let placesArray = []
+const validate = new Bouncer('form', { disableSubmit: true });
 
-// Get all data from database
-fetch(`https://tiny-lasagna-server.herokuapp.com/collections/mariaTestCollection/`)
-  .then((results) => {
-    return results.json();
-  })
-  .then((allDataFromServer) => {
-    console.log("This is everything in our collection!", allDataFromServer)
+async function sendDataToServer() {
 
-    placesArray = allDataFromServer;
-  });
+  let newPlace = {
+    name: $('#pname').val(),
+    description: $('#descriptioninput').val(),
+    address_1: $('#add1').val(),
+    address_2: $('#add2').val(),
+    hours: `${$('#day1 :selected').text()} - ${$('#day2 :selected').text()}, 
+    ${$('#time1').val()} - ${$('#time2').val()}`,
+    phone: $('#phone').val(),
+    website: $('#website').val(),
+    thumbnail: $('#image').val()
+  }
 
-$('#place-form').submit(function (event) {
-  console.log('Submit: click!')
-  event.preventDefault();
-
-  async function sendDataToServer() {
-
-    let newPlace = {
-      name: $('#pname').val(),
-      description: $('#descriptioninput').val(),
-      address_1: $('#add1').val(),
-      address_2: $('#add2').val(),
-      hours: $('#hours').val(),
-      phone: $('#phone').val(),
-      website: $('#website').val(),
-      thumbnail: $('#image').val()
+  const postOptions = {
+    method: "POST",
+    body: JSON.stringify(newPlace),
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     }
+  }
+  console.log('new place', newPlace)
 
-    const postOptions = {
-      method: "POST",
-      body: JSON.stringify(newPlace),
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }
-    console.log('new place', newPlace)
+  // Post new place to the server
+  const postRequest = await fetch('https://tiny-lasagna-server.herokuapp.com/collections/mariaTestCollection', postOptions);
 
-    // Post new place to the server
-    const postRequest = await fetch('https://tiny-lasagna-server.herokuapp.com/collections/mariaTestCollection', postOptions);
+  // Response from the server + id
+  const result = await postRequest.json();
 
-    // Response from the server + id
-    const result = await postRequest.json();
+  console.log("The id of my newly created post is", result._id);
 
-    console.log("The id of my newly created post is", result._id);
+  window.location.href = `file:///Users/maria/Documents/Coding%20Projects/Safely/profile.html?id=${result._id}`;
 
-    window.location.href = `file:///Users/maria/Documents/Coding%20Projects/Safely/profile.html?id=${result._id}`;
+};
 
-  };
-
-  sendDataToServer();
-});
+document.addEventListener('bouncerFormValid', sendDataToServer)
--- search.js
+++ search.js
@@ -1,7 +1,20 @@
 const searchButton = $('#searchbutton')
+const searchBar = $('#search');
+
+// Can trigger the search button with the Enter key. Trigger button on enter code from w3Schools:https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+
+// Execute a function when the user releases a key on the keyboard
+searchBar.keyup(function (event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    searchButton.click();
+  }
+});
 
 searchButton.click(function (event) {
-  const searchBar = $('#search');
   let searchTerm = searchBar.val();
 
   // Do not allow search on search button click if the searchbar is empty. 
